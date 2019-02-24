ef18a787022d33c5ac292e4c915f6cd5f9ffd0c6
mydeveloperday@gmail.com <mydeveloperday@gmail.com>
2019-02-24 21:47:56 +0000

    change the path to be dos path

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export CC=cl.exe
 export CXX=cl.exe
-cmake -G "CodeBlocks - Unix Makefiles" /cygdrive/c/projects/llvm-project/llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
+cmake -G "CodeBlocks - Unix Makefiles" c:/projects/llvm-project/llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
 make clang-format
 
 
