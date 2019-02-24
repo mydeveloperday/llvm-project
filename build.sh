45d3631ecaac19c14c7f0855f37bd137fd0ee279
mydeveloperday@gmail.com <mydeveloperday@gmail.com>
2019-02-24 22:31:01 +0000

    setup python path

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 export CC=cl.exe
 export CXX=cl.exe
-cmake -G "CodeBlocks - Unix Makefiles" c:/projects/llvm-project/llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
+cmake -G "CodeBlocks - Unix Makefiles" c:/projects/llvm-project/llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DPYTHON_EXECUTABLE:FILEPATH=c:\\Python27\\python.exe
+
 make clang-format
 
 
